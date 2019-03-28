8bb0890a6f562a3632a2fd7d33844fa3f5381dfc
Szymon Bernal <szymon.bernal@comarch.pl>
2019-03-28 18:48:36 +0100

    IO2019TEAM-7 Add Angular frontend

---
+++ src/app/example-module/example-module-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { CardModule } from 'primeng/card';
+import { ExampleComponentComponent } from './example-component/example-component.component';
+import { AnotherComponentComponent } from './another-component/another-component.component';
+
+const routes: Routes = [
+  { path: '', component: ExampleComponentComponent, pathMatch: 'full' },
+  { path: 'example', component: ExampleComponentComponent },
+  { path: 'another', component: AnotherComponentComponent }
+]
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    CardModule
+  ],
+  exports: [ RouterModule, CardModule ]
+})
+export class ExampleModuleRoutingModule { }
