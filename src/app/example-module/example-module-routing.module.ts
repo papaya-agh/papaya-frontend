8273820f83a5e6966a910bea32418a2fef0f86b8
Numernabis <luigi.zielone@gmail.com>
2019-04-20 10:11:32 +0200

    IO2019TEAM-54 Add menu bar sketch

--- src/app/example-module/example-module-routing.module.ts
+++ src/app/example-module/example-module-routing.module.ts
@@ -2,20 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { CardModule } from 'primeng/card';
+import { MenubarModule } from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
+
 import { ExampleComponentComponent } from './example-component/example-component.component';
+import { AnotherComponentComponent } from './another-component/another-component.component';
+import { BarComponent } from './bar/bar.component';
 
 const routes: Routes = [
   { path: '', component: ExampleComponentComponent, pathMatch: 'full' },
   { path: 'example', component: ExampleComponentComponent },
+  { path: 'another', component: AnotherComponentComponent },
+  { path: 'bar', component: BarComponent }
 ];
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    CardModule
+    CardModule,
+    MenubarModule,
+    ButtonModule
   ],
-  exports: [ RouterModule, CardModule ]
+  exports: [ RouterModule, CardModule, MenubarModule, ButtonModule ]
 })
 export class ExampleModuleRoutingModule {
 }
--- src/app/example-module/example-module.module.ts
+++ src/app/example-module/example-module.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExampleComponentComponent } from './example-component/example-component.component';
 import { ExampleModuleRoutingModule } from './example-module-routing.module';
+import { AnotherComponentComponent } from './another-component/another-component.component';
+import { BarComponent } from './bar/bar.component';
 
 @NgModule({
-  declarations: [ ExampleComponentComponent ],
+  declarations: [ ExampleComponentComponent, AnotherComponentComponent, BarComponent ],
   imports: [
     CommonModule,
     ExampleModuleRoutingModule
