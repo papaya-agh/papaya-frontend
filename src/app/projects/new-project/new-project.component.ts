49c0079b1830466cea6243d6a797a3b99804efc8
Numernabis <luigi.zielone@gmail.com>
2019-04-24 20:30:36 +0200

    IO2019TEAM-54 Apply proper code formatting

--- src/app/projects/new-project/new-project.component.ts
+++ src/app/projects/new-project/new-project.component.ts
@@ -3,11 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-new-project',
   templateUrl: './new-project.component.html',
-  styleUrls: ['./new-project.component.css']
+  styleUrls: [ './new-project.component.css' ]
 })
 export class NewProjectComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
