581304ee50ea4671670d6e0f0b6f8a1afe36e6b9
Maciej Klimowski <maciek.klimowski44@gmail.com>
2019-04-25 02:19:03 +0200

    IO2019TEAM-67 - Update main availability-table view

--- src/app/availability/availability-table/availability-table.component.ts
+++ src/app/availability/availability-table/availability-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-availability-table',
@@ -6,13 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./availability-table.component.css']
 })
 export class AvailabilityTableComponent implements OnInit {
-
-  constructor() { }
-
+  
+  constructor() {}
+  
   ngOnInit() {
   }
-
+  
   actualRate = 2;
   hoursSum = 123;
+  workers = [
+    { id: 1, name: "Robert Kubica", declaredHours: 3 },
+    { id: 2, name: "Adam Małysz", declaredHours: 15 },
+    { id: 3, name: "Krzysztof Krawczyk", declaredHours: 63 }
+  ];
+  sprint = "sprint 3";
 
 }
