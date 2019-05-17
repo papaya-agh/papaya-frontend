0aa59b1e1d3b70d2aed6a939ebde33c181ee3352
Szymon Bernal <szymon.bernal@gmail.com>
2019-05-17 21:43:44 +0200

    IO2019TEAM-113 Rewrite REST API specification to OpenAPI 3 format

--- generator/generate.js
+++ generator/generate.js
@@ -3,8 +3,8 @@ const exec = util.promisify(require('child_process').exec);
 const fs = require('fs-extra');
 const path = require('path');
 
-const genPath = path.join('./', 'node_modules', '.bin', 'ng-swagger-gen');
-const cfgPath = path.join('./', 'generator', 'config', 'ng-swagger-gen.json');
+const genPath = path.join('./', 'node_modules', '.bin', 'ng-openapi-gen');
+const cfgPath = path.join('./', 'generator', 'config', 'ng-openapi-gen.json');
 
 async function generateDeclarations() {
   const {stdout, stderr} = await exec(genPath + ' --config ' + cfgPath);
@@ -16,7 +16,6 @@ async function generateDeclarations() {
 
 async function cleanUp() {
   fs.copySync('generator/out/models', 'src/app/declarations/models');
-  fs.copySync('generator/out/services.ts', 'generator/server/server.js');
   fs.removeSync('generator/out');
 }
 
