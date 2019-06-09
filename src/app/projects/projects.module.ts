357dfbb13045b22758c385c9e483711f365f8a2b
Numernabis <luigi.zielone@gmail.com>
2019-06-09 19:53:26 +0200

    IO2019TEAM-135 Update sprint creation view

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -11,7 +11,7 @@ import { PCommonModule } from '../p-common/p-common.module';
 import { NewProjectComponent } from './new-project/new-project.component';
 import { ProjectMembersComponent } from './project-members/project-members.component';
 import { CardModule } from 'primeng/card';
-import { BlockUIModule, DropdownModule, KeyFilterModule, PanelModule } from 'primeng/primeng';
+import { BlockUIModule, DropdownModule, InputTextareaModule, InputTextModule, KeyFilterModule, PanelModule } from 'primeng/primeng';
 import { ToastModule } from 'primeng/toast';
 import { JiraKeyComponent } from './jira-key/jira-key.component';
 import { JiraConfigComponent } from './jira-config/jira-config.component';
@@ -36,6 +36,8 @@ import { JiraProjectsComponent } from './jira-projects/jira-projects.component';
     ToastModule,
     KeyFilterModule,
     PanelModule,
+    InputTextareaModule,
+    InputTextModule,
   ]
 })
 export class ProjectsModule {
